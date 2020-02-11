98506780a02bb24f735b4bcc1dabee16e56d19ce
Melissa Manousos <mmanousos@gmail.com>
2020-02-11 13:28:46 -0800

    Adds fetch and post, replies

--- client/src/components/CommentsForm.js
+++ client/src/components/CommentsForm.js
@@ -1,30 +1,63 @@
-import React from 'react';
+import React, { Component } from "react";
 
-const CommentsForm = () => {
-  return (
-    <form action="" onSubmit={this.handleSubmit}>
-      <h2>Post a Comment</h2>
-      <div className="input-group">
-        <label>Your Name</label>
-        <input
-          type="text"
-          name="author"
-          value=""
-        />
-      </div>
+class CommentsForm extends Component {
+  state = {
+    author: "",
+    body: ""
+  };
 
-      <div className="input-group">
-        <label>Your Comment</label>
-        <textarea
-          name="body"
-          value=""
-          cols="30"
-          rows="10"
-        ></textarea>
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
+  handleUserInput = e => {
+    e.preventDefault();
+    const inputName = e.target.name;
+
+    this.setState({
+      [inputName]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const comment = {
+      author: this.state.author,
+      body: this.state.body
+    };
+
+    this.props.onSubmit(comment);
+
+    this.setState({
+      author: "",
+      body: ""
+    });
+  };
+
+  render() {
+    return (
+      <form action="" onSubmit={this.handleSubmit}>
+        <h2>Post a Comment</h2>
+        <div className="input-group">
+          <label>Your Name</label>
+          <input
+            type="text"
+            name="author"
+            value={this.state.author}
+            onChange={this.handleUserInput}
+          />
+        </div>
+
+        <div className="input-group">
+          <label>Your Comment</label>
+          <textarea
+            name="body"
+            value={this.state.body}
+            cols="30"
+            rows="10"
+            onChange={this.handleUserInput}
+          ></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
 
 export default CommentsForm;
--- client/src/components/CommentsParent.js
+++ client/src/components/CommentsParent.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import Comment from './Comment';
+import React from "react";
+import Comment from "./Comment";
 
-const CommentsParent = (props) => {
-  const replies = props.comment.replies.map(reply => <Comment key={reply.id} comment={reply} />)
+const CommentsParent = props => {
+  const replies = props.comment.replies.map(reply => (
+    <Comment key={reply.id} comment={reply} />
+  ));
+  const handleMoreReplies = e => {
+    e.preventDefault();
+    props.onMoreReplies(props.comment.id);
+  };
+
   return (
     <div className="parent-comment">
       <Comment comment={props.comment} />
 
       <div className="replies">
         {replies}
-        <a href="" className="show_more">Show More Replies ({props.comment.replies_count - 1})</a>
+        {props.comment.replies_count !== replies.length ? (
+          <a href="" className="show_more" onClick={handleMoreReplies}>
+            Show More Replies ({props.comment.replies_count - 1})
+          </a>
+        ) : null}
       </div>
     </div>
   );
